2f003ded9a34116ea8eb1dc18786374dd4ade764
Steve Rushby <steve@rushby.com>
2023-03-04 20:23:25 +0000

    Save listing page progress

--- zenft/pages/listing/[listingId].tsx
+++ zenft/pages/listing/[listingId].tsx
@@ -1,132 +1,89 @@
-import {
-    useContract,
-    useNetwork,
-    useNetworkMismatch,
-} from "@thirdweb-dev/react";
-import {
-    AuctionListing,
-    DirectListing,
-    ListingType,
-} from "@thirdweb-dev/sdk";
+import { useContract } from "@thirdweb-dev/react";
+import { AuctionListing, DirectListing, ListingType } from "@thirdweb-dev/sdk";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { NextPage } from "next";
+import styles from "../../styles/Listing.module.css";
 
 type Listing = AuctionListing | DirectListing;
 
-const ListingsPage: NextPage = () => {
-    const networkMismatch = useNetworkMismatch();
-    const [, switchNetwork] = useNetwork();
-
-    const {contract} = useContract(
+const ListingPage = () => {
+    const router = useRouter();
+    const listingId = router.query.listingId as string;
+    const [listing, setListing] = useState<Listing | null>(null);
+    const { contract } = useContract(
         "0x5C075ef16255BF7a7F0c49A0a2f5c2BB325cd2f6",
         "marketplace"
     );
-    const [listings, setListings] = useState<Listing[]>([]);
-    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        async function getListings() {
+        async function fetchListing() {
             try {
-                setLoading(true);
-
-                if (networkMismatch) {
-                    switchNetwork && switchNetwork(1662);
-                    return;
-                }
-
                 const activeListings = await contract?.getActiveListings();
+                const matchingListing = activeListings?.find(
+                    (listing) => listing.id === listingId
+                );
 
-                setListings(activeListings || []);
-                setLoading(false);
+                if (matchingListing) {
+                    setListing(matchingListing);
+                } else {
+                    throw new Error(`Listing with ID ${listingId} not found`);
+                }
             } catch (error) {
                 console.error(error);
-                setLoading(false);
             }
         }
 
-        getListings();
-    }, [contract, networkMismatch, switchNetwork]);
+        fetchListing();
+    }, [contract, listingId]);
 
-    const createBidOrOffer = async (listingId: string, type: ListingType) => {
+    const createBidOrOffer = async (type: ListingType) => {
         try {
-            // Ensure user is on the correct network
-            if (networkMismatch) {
-                switchNetwork && switchNetwork(1662);
-                return;
-            }
-
-            // If the listing type is a direct listing, then we can create an offer.
             if (type === ListingType.Direct) {
-                await contract?.direct.makeOffer(
-                    listingId, // The listingId of the listing we want to make an offer for
-                    1, // Quantity = 1
-                    "0x1234567890123456789012345678901234567890", // Wrapped Ether address on Goerli
-                    "1000000000000000000" // The offer amount the user entered
-                );
+                // Create an offer for a direct listing
+            } else {
+                // Create a bid for an auction listing
             }
-
-            // If the listing type is an auction listing, then we can create a bid.
-            if (type === ListingType.Auction) {
-                await contract?.auction.makeBid(listingId, "1000000000000000000");
-            }
-
-            alert(`${type === ListingType.Auction ? "Bid" : "Offer"} created successfully!`);
         } catch (error) {
             console.error(error);
-            alert(error);
         }
     };
 
-    const buyNft = async (listingId: string) => {
+    const buyNft = async () => {
         try {
-            // Ensure user is on the correct network
-            if (networkMismatch) {
-                switchNetwork && switchNetwork(1662);
-                return;
-            }
-
-            // Simple one-liner for buying the NFT
-            await contract?.buyoutListing(listingId, 1);
-            alert("NFT bought successfully!");
+            // Buy the NFT
         } catch (error) {
             console.error(error);
-            alert(error);
         }
     };
 
     return (
         <div>
-            <h1>Listings</h1>
-
-            {loading && <p>Loading...</p>}
-
-            {!loading && listings?.length === 0 && (
-                <p>No listings found</p>
-            )}
-
-            {!loading && listings?.length > 0 && (
+            {listing ? (
                 <div>
-                    {listings.map((listing: any) => (
-                        <div key={listing.listingId}>
-                            <img src={listing.asset.image}/>
-                            <h2>{listing.asset.name}</h2>
-                            <p>{listing.asset.description}</p>
-                            <p>{listing.sellerAddress}</p>
-                            <p>
-                                {listing.type === ListingType.Direct
-                                    ? "Direct Listing"
-                                    : "Auction Listing"}
-                            </p>
-                            <p>
-                                {listing.buyoutCurrencyValuePerToken.displayValue}{" "}
-                                {listing.buyoutCurrencyValuePerToken.symbol}
-                            </p>
-                        </div>
-                    ))}
+                    <h1 className={styles["listing-page-h1"]}>{listing.asset.name}</h1>
+                    <div className={styles["listing-container-large"]}>
+                        <img src={listing.asset.image ?? "default-image-url"} alt={String(listing.asset.name) ?? "default-image-alt"} />
+                        <h2>{listing.asset.name}</h2>
+                        <p>{listing.asset.description}</p>
+                        <p>{listing.sellerAddress}</p>
+                        <p>
+                            {listing.type === ListingType.Direct
+                                ? "Direct Listing"
+                                : "Auction Listing"}
+                        </p>
+                        <p>
+                            {listing.buyoutCurrencyValuePerToken.displayValue}{" "}
+                            {listing.buyoutCurrencyValuePerToken.symbol}
+                        </p>
+                        <button onClick={() => createBidOrOffer(listing.type)}>Bid/Buy</button>
+                        <button onClick={() => buyNft()}>Buy Now</button>
+                    </div>
                 </div>
+            ) : (
+                <p>Loading listing...</p>
             )}
         </div>
     );
-}
+};
 
-export default ListingsPage;
+export default ListingPage;
